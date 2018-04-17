42f607258dff3f8d211373782c20c4d7f98b60c2
cgoldsby <cgoldsby@gmail.com>
2018-04-17 18:56:51 -0400

    Update nose position

--- LoginCritter/Sources/Login/Critter/Parts/Nose.swift
+++ LoginCritter/Sources/Login/Critter/Parts/Nose.swift
@@ -10,17 +10,17 @@ import UIKit
 
 final class Nose: UIImageView, CritterAnimatable {
 
+    private let p1 = CGPoint(x: 22.2, y: 2.2)
+    private let p2 = CGPoint(x: 12.6, y: 5.2)
+
     convenience init() {
         self.init(image: UIImage.Critter.nose)
-        frame = CGRect(x: 22.2, y: 2.2, width: 13, height: 9.6)
+        frame = CGRect(x: p1.x, y: p1.y, width: 13, height: 9.6)
     }
 
     // MARK: - CritterAnimatable
 
     func applyActiveStartState() {
-        let p1 = CGPoint(x: 22.2, y: 2.2)
-        let p2 = CGPoint(x: 13.2, y: 5.2)
-
         layer.transform = CATransform3D
             .identity
             .translate(.x, by: p2.x - p1.x)
@@ -28,9 +28,6 @@ final class Nose: UIImageView, CritterAnimatable {
     }
     
     func applyActiveEndState() {
-        let p1 = CGPoint(x: 22.2, y: 2.2)
-        let p2 = CGPoint(x: 13.2, y: 5.2)
-
         layer.transform = CATransform3D
             .identity
             .translate(.x, by: -(p2.x - p1.x))

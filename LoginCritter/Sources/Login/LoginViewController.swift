8d9264f787cc21e866699b52d52de236b4d0c676
cgoldsby <cgoldsby@gmail.com>
2018-04-18 08:31:16 -0400

    Add email text field!

--- LoginCritter/Sources/Login/LoginViewController.swift
+++ LoginCritter/Sources/Login/LoginViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 private let critterViewDimension: CGFloat = 160
 private let critterViewTopMargin: CGFloat = 70
+private let emailTextFieldTopMargin: CGFloat = 38.8
+private let emailTextFieldWidth: CGFloat = 206
+private let emailTextFieldHeight: CGFloat = 37
 
-final class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController, UITextFieldDelegate {
 
     private let critterView = CritterView(frame: CGRect(x: 0, y: 0, width: critterViewDimension, height: critterViewDimension))
+
+    private lazy var emailTextField: UITextField = {
+        let view = UITextField(frame: CGRect(x: 0, y: 0, width: emailTextFieldWidth, height: emailTextFieldHeight))
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5
+        view.tintColor = Colors.dark
+        view.placeholder = "Email"
+        view.keyboardType = .emailAddress
+        view.autocorrectionType = .no
+        view.autocapitalizationType = .none
+        view.spellCheckingType = .no
+        view.delegate = self
+        view.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        return view
+    }()
 
     deinit {
         NotificationCenter.default.removeObserver(self)
@@ -24,12 +42,35 @@ final class LoginViewController: UIViewController {
         setUpView()
     }
 
+    // MARK: - UITextFieldDelegate
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == emailTextField {
+            let deadlineTime = DispatchTime.now() + .milliseconds(100)
+            DispatchQueue.main.asyncAfter(deadline: deadlineTime) { // 🎩✨ Magic to ensure animation starts
+                let fractionComplete = self.fractionComplete(for: textField)
+                self.critterView.startHeadRotation(startAt: fractionComplete)
+            }
+        }
+    }
+
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        let fractionComplete = self.fractionComplete(for: textField)
+        critterView.updateHeadRotation(to: fractionComplete)
+
+        if let text = textField.text, text.range(of: "@") != nil {
+            critterView.validateAnimation()
+        }
+    }
+
     // MARK: - Private
 
     private func setUpView() {
         view.backgroundColor = Colors.dark
         view.addSubview(critterView)
+        view.addSubview(emailTextField)
         setUpCritterViewConstraints()
+        setUpEmailTextFieldConstraints()
         setUpNotification()
 
         dev_setUpTestUI()
@@ -43,12 +84,25 @@ final class LoginViewController: UIViewController {
         critterView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: critterViewTopMargin).isActive = true
     }
 
+    private func setUpEmailTextFieldConstraints() {
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.heightAnchor.constraint(equalToConstant: emailTextFieldHeight).isActive = true
+        emailTextField.widthAnchor.constraint(equalToConstant: emailTextFieldWidth).isActive = true
+        emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: critterView.bottomAnchor, constant: emailTextFieldTopMargin).isActive = true
+    }
+
     private func setUpNotification() {
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackground), name: NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
     }
 
     @objc private func applicationDidEnterBackground() {
         dev_neutralAnimation()
+    }
+
+    private func fractionComplete(for textField: UITextField) -> Float {
+        guard let text = textField.text, let font = textField.font else { return 0 }
+        return min(Float(text.size(withAttributes: [NSAttributedStringKey.font : font]).width / textField.bounds.width), 1)
     }
 
     // MARK: - Dev
@@ -85,13 +139,13 @@ final class LoginViewController: UIViewController {
             ]
         )
         stackView.axis = .vertical
-        stackView.spacing = 15
+        stackView.spacing = 5
         view.addSubview(stackView)
 
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25).isActive = true
         stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
@@ -101,6 +155,7 @@ final class LoginViewController: UIViewController {
     }
 
     @objc private func dev_neutralAnimation() {
+        emailTextField.resignFirstResponder()
         critterView.stopHeadRotation()
         activeAnimationSlider.isEnabled = false
     }
